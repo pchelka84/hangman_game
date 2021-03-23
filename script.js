0064102a0d21b93180d7d95c38149e341861f03d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-23 11:14:30 -0400

    Draw parts, check if a user lost

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const wordEl = document.getElementById('word');
-const wrongLettersEl = document.getElementById('wrong-letters');
+const wrongLettersEl = document.getElementById('wrong-letters');;
 const playAgainBtn = document.getElementById('play-again');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
 
-const figurePart = document.querySelectorAll('.figure-part');
+const figureParts = document.querySelectorAll('.figure-part');
 
 const words = ['application', 'programming','interface', 'wizard'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ['a', 'p', 'l', 'i', 'c', 'a', 't', 'o', 'n'];
+const correctLetters = ['a', 'p', 'l', 'c', 'a', 't', 'o', 'n'];
 const wrongLetters = [];
 
 // Show hidden word
@@ -31,14 +31,35 @@ function displayWord() {
  const innerWord = wordEl.innerText.replace(/\n/g,'')
 
  if (innerWord === selectedWord) {
-   finalMessage.innerText='Congratulations! You won!';
+   finalMessage.innerText='Congratulations! You won! 😃';
    popup.style.display = 'flex';
  }
 }
 
 // Update worng letter 
 function updateWrongLettersEl() {
-  console.log('update worng');
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  })
+
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunately, you lost. 😕"
+    popup.style.display = "flex";
+  }
 }
 
 // Show notification
