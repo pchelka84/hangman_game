f0f74c6f29b71f786eacde8c2c710bce439380e7
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-23 10:41:24 -0400

    Add keydown Handle keydown letter press, set up function calls

--- script.js
+++ script.js
@@ -36,4 +36,30 @@ function displayWord() {
  }
 }
 
+// Keydown letter press
+window.addEventListener('keydown', e => {
+  // console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+})
+
 displayWord();
